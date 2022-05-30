526d5a00c983a99810d93b0c624662407bd38e76
sembhip <p.sembhi@elsevier.com>
2022-05-30 15:20:33 +0100

    fixed document query. total citation count now works for author and document

--- src/main/java/MetricReviewer.java
+++ src/main/java/MetricReviewer.java
@@ -10,22 +10,17 @@ import java.sql.SQLOutput;
 public class MetricReviewer {
     private static final Logger LOGGER = LoggerFactory.getLogger(MetricReviewer.class);
 
-    public static void main( String... args ) throws Exception
-    {
-        try ( Neo4jClient client = new Neo4jClient( "bolt://neo4j-cluster.dev.scopussearch.net:7687", "neo4j", "changeme", true, "full-14-april" ) )
-        {
+    public static void main( String... args ) throws Exception {
+        try (Neo4jClient client = new Neo4jClient("bolt://neo4j-cluster.dev.scopussearch.net:7687", "neo4j", "changeme", true, "full-14-april")) {
             Author author = new Author(client);
             int totalCitedBy = author.totalCitedBy("7005886441");
             System.out.println("Total cited by: " + totalCitedBy);
 
-        }
-        try ( Neo4jClient client = new Neo4jClient( "bolt://neo4j-cluster.dev.scopussearch.net:7687", "neo4j", "changeme", true, "full-14-april" ) )
-        {
-
             Document document = new Document(client);
-            int totalCitedBy = document.totalCitedBy("0024937865");
-            System.out.println("Total cited by: " + totalCitedBy);
+            int totalDocCitedBy = document.totalDocCitedBy("0024937865");
+            System.out.println("Total cited by: " + totalDocCitedBy);
         }
     }
+
 }
 
--- src/main/java/com/elsevier/apiimplementationreviewer/query/Document.java
+++ src/main/java/com/elsevier/apiimplementationreviewer/query/Document.java
@@ -10,11 +10,13 @@ public class Document {
     private Neo4jClient neo4j;
 
     public Document (Neo4jClient neo4j) {this.neo4j = neo4j;}
-    public int totalCitedBy(String id)
+    public int totalDocCitedBy(String id)
     {
-        Record docCount = this.neo4j.executeSingleQuery("MATCH (:Work {ID:$ID})-[:references]->()<-[:refernces]-(citingDoc) RETURN COUNT(DISTINCT citingDoc) AS result;", Map.of("ID", id));
+        Record docCount = this.neo4j.executeSingleQuery("MATCH (n:Work {ID:$ID}) RETURN SIZE((n)<-[:references]-()) AS result;", Map.of("ID", id)); //we need return size for a work
         return docCount.get("result").asInt();
     }
 }
+
+
 
 
