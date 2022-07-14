8cbadc4a7f235c695131e3df2e47cb8d28b60ce4
sembhip <p.sembhi@elsevier.com>
2022-07-14 10:47:48 +0100

    revert package

--- src/main/java/com/elsevier/apiimplementationreviewer/query/Document.java
+++ src/main/java/com/elsevier/apiimplementationreviewer/query/Document.java
@@ -1,7 +1,7 @@
-package com.elsevier.apiimplementationreviewer.helper.query;
+package com.elsevier.apiimplementationreviewer.query;
 
 import com.elsevier.apiimplementationreviewer.helper.Neo4jClient;
-import com.elsevier.apiimplementationreviewer.helper.metrics.DocumentMetric;
+import com.elsevier.apiimplementationreviewer.metrics.DocumentMetric;
 import org.neo4j.driver.Record;
 
 import java.util.Map;
