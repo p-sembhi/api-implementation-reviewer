8c50394452e37c4430681a0ed855ae9c921be8a5
sembhip <p.sembhi@elsevier.com>
2022-07-14 10:50:26 +0100

    revert package

--- src/main/java/com/elsevier/apiimplementationreviewer/csv/DocumentCSVGenerator.java
+++ src/main/java/com/elsevier/apiimplementationreviewer/csv/DocumentCSVGenerator.java
@@ -1,11 +1,11 @@
-package com.elsevier.apiimplementationreviewer.helper.csv;
+package com.elsevier.apiimplementationreviewer.csv;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 // This class extends functionality from CSVGenerator (parent class) and creates the header for document csv file
-public class DocumentCSVGenerator extends CSVGenerator{
+public class DocumentCSVGenerator extends CSVGenerator {
 
     public DocumentCSVGenerator(BufferedOutputStream output) throws IOException {
         this.output = output;
